6b01de9914c8a950d5ec000ce08e26f2b75ccb19
faizallmaullana <faizalmaulanasekias@gmail.com>
2025-04-27 22:54:58 +0700

    feat: adding phone

--- dto/user-dto.go
+++ dto/user-dto.go
@@ -5,17 +5,16 @@ import (
 )
 
 type UserResponseDTO struct {
-	ID             uint64 `json:"id"`
-	Email          string `json:"email"`
-	Role           string `json:"role"`
-	Name           string `json:"name"`
-	Major          string `json:"major"`
-	ProfilePicture string `json:"profile_picture"`
-	// Gender     string     `json:"gender"`
-	NIM string `json:"nim"`
-	// BirthPlace string     `json:"birth_place"`
-	BirthDate *time.Time `json:"birth_date"`
-	Institusi string     `json:"institusi"`
+	ID             uint64     `json:"id"`
+	Email          string     `json:"email"`
+	Role           string     `json:"role"`
+	Name           string     `json:"name"`
+	Major          string     `json:"major"`
+	ProfilePicture string     `json:"profile_picture"`
+	NIM            string     `json:"nim"`
+	BirthDate      *time.Time `json:"birth_date"`
+	Institusi      string     `json:"institusi"`
+	Phone          string     `json:"phone"`
 
 	ProfileHasUpdated bool `json:"profile_has_updated"`
 	DataHasVerified   bool `json:"data_has_verified"`
@@ -25,6 +24,7 @@ type UpdateUserProfileDTO struct {
 	Name       string `json:"name" binding:"required"`
 	Gender     string `json:"gender" binding:"required"`
 	NIM        string `json:"nim" binding:"required"`
+	Phone        string `json:"phone"`
 	BirthPlace string `json:"birth_place" binding:"required"`
 	BirthDate  string `json:"birth_date" binding:"required"`
 	Institusi  string `json:"institusi" binding:"required"`
